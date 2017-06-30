2b07d4e6cd3e8994ac46c2331344f335a0e141fa
Dennis Aldea <dennis.aldea@gmail.com>
2017-06-30 14:59:35 -0400

    Add analysis interface
    The analysis interface parses command line options and validates user
    input before running BETA and 'analysis-engine.r'

---
+++ analysis-interface.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#===============================================================================
+# TITLE    : analysis-interface.sh
+# ABSTRACT : A BASH script that validates command line arguments before passing
+#            them to BETA and 'analysis-engine.sh'
+#
+# AUTHOR   : Dennis Aldea <dennis.aldea@gmail.com>
+# DATE     : 2017-06-30
+#
+# LICENCE  : MIT <https://opensource.org/licenses/MIT>
+#-------------------------------------------------------------------------------
+# USAGE:
+#
+#     ghmtools analysis [OPTIONS] RNA_DATA CHIP_DATA BINDING_DISTANCE GENE_LIST
+#         CSV_FILE
+#
+# OPTIONS:
+#
+#     -f                : do not prompt before overwriting files
+#     -i                : prompt before overwriting files (default)
+#     -n                : do not overwrite files
+#     --window [NUMBER] : number of genes to be summed to calculate a binding
+#                         score (defaults to 10 if not specified)
+#
+#     If conflicting options are given, the last option given takes effect.
+#
+# ARGUMENTS:
+#
+#     RNA_DATA         : filepath of the file containing RNA-seq data
+#     CHIP_DATA        : filepath of the file containing ChIP-seq data
+#     BINDING_DISTANCE : maximum distance (in kilobases) that a gene can be from
+#                        a binding site to be listed as a bound gene
+#     GENE_LIST        : filepath where the list of bound genes will be saved
+#     CSV_FILE         : filepath where the gene activity CSV file will be saved
+#===============================================================================
