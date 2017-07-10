1e462f6a51e76dc0b5728da2b2aeb990ed66ffb3
Dennis Aldea <dennis.aldea@gmail.com>
2017-07-10 10:05:29 -0400

    add code to ignore gene names in input data

--- heatmap-engine.r
+++ heatmap-engine.r
@@ -6,19 +6,19 @@
 #            heatmaps from CSV files
 #
 # AUTHOR   : Dennis Aldea <dennis.aldea@gmail.com>
-# DATE     : 2017-06-28
+# DATE     : 2017-07-10
 #
 # LICENCE  : MIT <https://opensource.org/licenses/MIT>
 #-------------------------------------------------------------------------------
 # USAGE:
 #
-#     ./heatmap-engine.r CSV_PATH INCLUDE_ZEROS TRANSCRIPTION_MIN
+#     ./heatmap-engine.r GENE_PATH INCLUDE_ZEROS TRANSCRIPTION_MIN
 #         TRANSCRIPTION_MAX BINDING_MAX TRANSCRIPTION_PATH BINDING_PATH
 #
 # ARGUMENTS:
 #
-#     CSV_PATH           : filepath of the CSV file containing gene
-#                          transcription and gene binding data
+#     GENE_PATH          : filepath of the file containing gene transcription
+#                          and gene binding data
 #     INCLUDE_ZEROS      : TRUE  -> map genes with zero transcription values
 #                          FALSE -> do not map genes with zero transcription
 #                                   values
@@ -41,6 +41,84 @@ options(warn = -1)
 library(ggplot2)
 library(svglite)
 options(warn = default_warn)
+
+# store command line arguments into a list with given names and convert numeric
+# strings to doubles
+store_arguments <- function(name_vector) {
+    # store command line arguments in a list
+    argument_list <- as.list(commandArgs(trailingOnly = TRUE))
+    for (index in c(1:length(argument_list))) {
+        # if argument is a numeric string, convert it to double
+        if (suppressWarnings(!is.na(as.double(argument_list[index])))) {
+            argument_list[index] <- as.double(argument_list[index])
+        }
+    }
+    names(argument_list) <- name_vector
+    return(argument_list)
+}
+
+# generate a copy of a data frame in which columns containing non-numeric data
+# are removed
+extract_numeric_columns <- function(data) {
+    # copy data into a local variable so that original data is not modified
+    local_data <- data
+    # determine columns that only numeric data from logical vector
+    is_numeric_vector <- sapply(local_data, is.numeric)
+    numeric_indexes_vector <- which(is_numeric_vector)
+    # remove non-numeric columns
+    local_data <- local_data[numeric_indexes_vector]
+    return(local_data)
+}
+
+# generate a copy of a data frame in which all values in a given column greater
+# than a given max are set to max and all values less than a given min are set
+# to min
+# the data are sorted by an optional sort column and a given subset of columns
+# are extracted to form the output data frame
+flatten_outliers <- function(data, test, min, max, sort = NULL,
+                             selection = NULL) {
+    # copy data into a local variable so that original data is not modified
+    local_data <- data
+    for (row_index in c(1:nrow(local_data))) {
+        # flatten data in test column (set outliers to min or max)
+        if (local_data[row_index, test] < min) {
+            local_data[row_index, test] <- min
+        } else if (local_data[row_index, test] > max) {
+            local_data[row_index, test] <- max
+        }
+    }
+    # sort in ascending order by sort column, if given
+    if (!is.null(sort)) {
+        local_data <- local_data[order(local_data[[sort]]), ]
+    }
+    # create a subset using selected columns, if given
+    if (!is.null(selection)) {
+        local_data <- local_data[selection]
+    }
+    # reset row names (1, 2, 3, ... nrows)
+    rownames(local_data) <- NULL
+    return(local_data)
+}
+
+# append a column of meaningless y values to a data frame
+# reference: <https://stackoverflow.com/a/21911221>
+expand_grid_df <- function(...) {
+    Reduce(function(...) merge(..., by = NULL), list(...))
+}
+
+# save a plot with a given scale, theme and dimensions (cm) to a given file
+draw_heatmap <- function(ggplot, fill_scale, theme,
+                         dimension_vector, filepath) {
+    map <- ggplot + geom_raster() +
+                    scale_x_continuous(expand = c(0,0)) +
+                    scale_y_continuous(expand = c(0,0)) +
+                    fill_scale +
+                    theme
+    ggsave(filepath,
+           plot   = map,
+           width  = dimension_vector[2],
+           height = dimension_vector[1])
+}
 
 # height and width of images in centimeters
 image_dimensions <- c(5, 15)
@@ -71,71 +149,6 @@ black_yellow_scale <- scale_fill_gradient(labels = function(x)
                                           high   = "yellow",
                                           guide  = "colorbar")
 
-# save a plot with a given scale, theme and dimensions (cm) to a given file
-draw_heatmap <- function(ggplot, fill_scale, theme,
-                         dimension_vector, filepath) {
-    map <- ggplot + geom_raster() +
-                    scale_x_continuous(expand = c(0,0)) +
-                    scale_y_continuous(expand = c(0,0)) +
-                    fill_scale +
-                    theme
-    ggsave(filepath,
-           plot   = map,
-           width  = dimension_vector[2],
-           height = dimension_vector[1])
-}
-
-# append a column of meaningless y values to a data frame
-# reference: <https://stackoverflow.com/a/21911221>
-expand_grid_df <- function(...) {
-    Reduce(function(...) merge(..., by = NULL), list(...))
-}
-
-# generate a copy of a data frame in which all values in a given column greater
-# than a given max are set to max and all values less than a given min are set
-# to min
-# the data are sorted by an optional sort column and a given subset of columns
-# are extracted to form the output data frame
-flatten_outliers <- function(data, test, min, max, sort = NULL,
-                             selection = NULL) {
-    # copy data into a local variable so that original data is not modified
-    modified_data <- data
-    for (row_index in c(1:nrow(modified_data))) {
-        # flatten data in test column (set outliers to min or max)
-        if (modified_data[row_index, test] < min) {
-            modified_data[row_index, test] <- min
-        } else if (modified_data[row_index, test] > max) {
-            modified_data[row_index, test] <- max
-        }
-    }
-    # sort in ascending order by sort column, if given
-    if (!is.null(sort)) {
-        modified_data <- modified_data[order(modified_data[[sort]]), ]
-    }
-    # create a subset using selected columns, if given
-    if (!is.null(selection)) {
-        modified_data <- subset(modified_data, select = selection)
-    }
-    # reset row names (1, 2, 3, ... nrows)
-    rownames(modified_data) <- NULL
-    return(modified_data)
-}
-
-# store command line arguments into a list with given names and convert numeric
-# strings to doubles
-store_arguments <- function(name_vector) {
-    # store command line arguments in a list
-    argument_list <- as.list(commandArgs(trailingOnly = TRUE))
-    for (index in c(1:length(argument_list))) {
-        # if argument is a numeric string, convert it to double
-        if (suppressWarnings(!is.na(as.double(argument_list[index])))) {
-            argument_list[index] <- as.double(argument_list[index])
-        }
-    }
-    names(argument_list) <- name_vector
-    return(argument_list)
-}
-
 # read arguments from command line
 argument_names <- c("csv_path", "include_zeros", "transcription_min",
                     "transcription_max", "binding_max", "transcription_path",
@@ -144,6 +157,8 @@ args <- store_arguments(argument_names)
 
 # read data from CSV
 gene_data <- read.delim(args[["csv_path"]], header = FALSE)
+# remove columns with non-numeric data (i.e. gene names)
+gene_data <- extract_numeric_columns(gene_data)
 colnames(gene_data) <- c("transcription", "binding")
 
 # remove genes with zero transcription values, if requested
